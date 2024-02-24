d4d721bfa818001e0c85ac81530cee3a614a0fb5
Hank0670devdev <161090059+Hank0670devdev@users.noreply.github.com>
2024-02-25 07:22:26 +1000

    Update login.py

--- login.py
+++ login.py
@@ -1,49 +1,4 @@
-import getpass
-import click  # Importing click for CLI commands
-
-# Define your OS commands as Click commands
-@click.group()
-def os_cli():
-    pass
-
-@os_cli.command()
-def files():
-    """View files in the OS."""
-    click.echo("Listing files in the OS:")
-    # Add code here to list files in the OS directory
-
-@os_cli.command()
-def open_file():
-    """Open a file."""
-    filename = click.prompt("Enter the name of the file to open")
-    # Add code here to open the specified file
-
-@os_cli.command()
-def processes():
-    """View running processes."""
-    click.echo("Listing running processes:")
-    # Add code here to list running processes
-
-@os_cli.command()
-def execute_process():
-    """Execute a process."""
-    process_name = click.prompt("Enter the name of the process to execute")
-    # Add code here to execute the specified process
-
-@os_cli.command()
-def logout():
-    """Logout of the OS."""
-    click.echo("Logging out...")
-    # Add code here to logout
-
-# Authenticate function remains the same
-
-def sign_up():
-    username = input("Enter a new username: ")
-    password = getpass.getpass("Enter a new password: ")
-    with open('credentials.txt', 'a') as file:
-        file.write(f"{username},{password}\n")
-    print("Sign up successful!")
+from os_module import OS  # Importing the OS class from the os_module module
 
 def main():
     while True:
@@ -58,8 +13,30 @@ def main():
             password = getpass.getpass("password: ")
             if authenticate(username, password):
                 print("Login successful!")
-                os_cli()  # Launch the OS CLI after successful login
-                break  # Exit the loop after logout
+                os_instance = OS()  # Creating an instance of the OS class
+                while True:
+                    print("\n<1> View Files")
+                    print("<2> Open File")
+                    print("<3> List Processes")
+                    print("<4> Execute Process")
+                    print("<5> Logout")
+                    choice = input(">>> ")
+                    if choice == "1":
+                        os_instance.view_files()
+                    elif choice == "2":
+                        filename = input("Enter the name of the file to open: ")
+                        os_instance.open_file(filename)
+                    elif choice == "3":
+                        os_instance.list_processes()
+                    elif choice == "4":
+                        command = input("Enter the command to execute: ")
+                        os_instance.execute_process(command)
+                    elif choice == "5":
+                        print("Logging out...")
+                        break
+                    else:
+                        print("Invalid choice. Please choose again.")
+                break
             else:
                 print("Invalid username or password.")
         else:
